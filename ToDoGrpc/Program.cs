aff83f6da7233d31b053779068175df13e58b681
Hihiz <public@mail.ru>
2023-08-11 23:47:46 +0300

    Добавление grpc service

--- ToDoGrpc/Program.cs
+++ ToDoGrpc/Program.cs
@@ -9,11 +9,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ConnectionSqlite"));
 });
 
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ToDoService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
